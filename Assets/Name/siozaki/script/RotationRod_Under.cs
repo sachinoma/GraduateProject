26f3f658d0be636d591e3f72ae44ea2b2bdd142d
sio <22ci0320@jec.ac.jp>
2023-09-27 11:58:36 +0900

    柱追加

---
+++ Assets/Name/siozaki/script/RotationRod_Under.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationRod_Under: MonoBehaviour
+{
+    void Update()
+    {
+        //オブジェクトを回転
+        transform.Rotate(new Vector3(0, 0.1f, 0));
+    }
+}
