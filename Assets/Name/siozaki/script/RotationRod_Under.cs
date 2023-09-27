57e1e98c85dce242367cd85f75327fc174fc3731
sio <22ci0320@jec.ac.jp>
2023-09-27 12:29:57 +0900

    柱の回転速度変化

--- Assets/Name/siozaki/script/RotationRod_Under.cs
+++ Assets/Name/siozaki/script/RotationRod_Under.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class RotationRod_Under: MonoBehaviour
 {
+    private float RY = 0.1f;
+    private int SpeedUpCnt = 0;
     void Update()
     {
+        //回転速度上昇
+        if(this.SpeedUpCnt > 600)
+        {
+            this.RY += 0.01f;
+            this.SpeedUpCnt = 0;
+        }
         //オブジェクトを回転
-        transform.Rotate(new Vector3(0, 0.1f, 0));
+        transform.Rotate(new Vector3(0, RY, 0));
+        //回転速度上昇カウンタ
+        this.SpeedUpCnt++;
     }
 }
