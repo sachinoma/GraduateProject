a787d7ecacde41506e87d1c0faeb9c1b9ef72cdb
sio <22ci0320@jec.ac.jp>
2023-09-29 14:39:19 +0900

    動く壁　制作途中　save

--- Assets/Name/siozaki/script/Pillar/RotationRod_Under.cs
+++ Assets/Name/siozaki/script/Pillar/RotationRod_Under.cs
@@ -8,15 +8,15 @@ public class RotationRod_Under: MonoBehaviour
     private int SpeedUpCnt = 0;
     void Update()
     {
+        //回転速度上昇カウンタ
+        this.SpeedUpCnt++;
         //回転速度上昇
-        if(this.SpeedUpCnt > 600)
+        if (this.SpeedUpCnt > 600)
         {
             this.RY += 0.01f;
             this.SpeedUpCnt = 0;
         }
         //オブジェクトを回転
         transform.Rotate(new Vector3(0, RY, 0));
-        //回転速度上昇カウンタ
-        this.SpeedUpCnt++;
     }
 }
