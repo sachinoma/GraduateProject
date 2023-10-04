338287eb6f87e49e72ba51133f9886876c055e1a
sio <22ci0320@jec.ac.jp>
2023-10-04 10:02:48 +0900

    inspector内で棒の回転速度変数を変更できるようにした。

--- Assets/Name/siozaki/script/Pillar/RotationRod_Up.cs
+++ Assets/Name/siozaki/script/Pillar/RotationRod_Up.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class RotationRod_UP : MonoBehaviour
 {
+    [SerializeField]
+    private float RotateSpeed;
     // Update is called once per frame
     void Update()
     {
         //オブジェクトを回転
-        transform.Rotate(new Vector3(0, 0.1f, 0));
+        transform.Rotate(new Vector3(0, RotateSpeed, 0));
     }
 }
