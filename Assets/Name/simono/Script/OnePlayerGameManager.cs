63584b6655927278d302b0693d84006cb4dc6b17
ci <ci@jec.ac.jp>
2023-10-10 10:25:30 +0900

    シーン遷移と時間を渡す処理を実装

--- Assets/Name/simono/Script/OnePlayerGameManager.cs
+++ Assets/Name/simono/Script/OnePlayerGameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem.Processors;
 
@@ -7,8 +8,9 @@ public class OnePlayerGameManager : MonoBehaviour
 {
     [SerializeField] private float gameTimer;
     private bool isDeath;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         isDeath = false;
         gameTimer = 0.0f;
@@ -31,8 +33,22 @@ public class OnePlayerGameManager : MonoBehaviour
 
         //エフェクト出す
 
+        // イベントに登録
+        SceneManager.sceneLoaded += ResultSceneLoaded;
+
         //シーン遷移
+        SceneManager.LoadScene("Result");
     }
 
-    
+    private void ResultSceneLoaded(Scene next, LoadSceneMode mode)
+    {
+        var gameManager = GameObject.FindWithTag("GameManager").GetComponent<OnePlayerGameManager>();
+
+        gameManager.gameTimer = gameTimer;
+        gameManager.isDeath = isDeath;
+
+        //イベントを削除
+        SceneManager.sceneLoaded -= ResultSceneLoaded;
+    }
+
 }
