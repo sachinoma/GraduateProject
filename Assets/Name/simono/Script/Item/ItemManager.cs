4b9530c24d9f65df73125e9c453b4eef3be3d9e5
ci <ci@jec.ac.jp>
2023-11-21 11:18:54 +0900

    ランダムリスポーンに修正

--- Assets/Name/simono/Script/Item/ItemManager.cs
+++ Assets/Name/simono/Script/Item/ItemManager.cs
@@ -1,59 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
-[RequireComponent(typeof(Collision))]
 public class ItemManager : MonoBehaviour
 {
-    static string PlayerTag = "Player";
+    static ItemManager instance;
+    public static ItemManager Instance { get { return instance; } }
 
     [SerializeField] float respawnTime = 5f;
-    [SerializeField] GameObject destroyParticle;
-    private Renderer[] renderlist;
-    private AudioSource audioSource;
-
-    private void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-        renderlist =  transform.GetComponentsInChildren<Renderer>();
-    }
-
-    public bool DetectPlayer(Collider _target)
-    {
-        return _target.CompareTag(PlayerTag);
-    }
-
-    public void CallCreateParticle()
-    {
-        if(destroyParticle != null)
-            Instantiate(destroyParticle, transform);
-    }
-
-    public void CallPlaySound()
-    {
-        if (audioSource != null && audioSource.clip != null)
-            audioSource.PlayOneShot(audioSource.clip);
+    [SerializeField] bool isRandom = true;
+    [SerializeField] List<GameObject> ItemPrefab;
         
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
     }
 
-    public void CallDestroy()
+    int GetListNum(GameObject _target, List<GameObject> _list)
     {
-        Destroy(gameObject);
+        var state = _target.GetComponentInChildren<Item>().GetState();
+        for (int i = 0; i < _list.Count; i++)
+        {
+            if (state == _list[i].GetComponentInChildren<Item>().GetState())
+            {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
-    public void CallRespawn()
+    public void Respawn(GameObject _target)
     {
-        StartCoroutine(Respawn(respawnTime, gameObject));
+        Vector3 respawnPos = _target.transform.position;
+
+       int itemNum = GetListNum(_target, ItemPrefab);
+        if(itemNum < 0) { return; }
+
+        Destroy(_target);
+
+        StartCoroutine(RespawnItem(respawnTime, respawnPos, itemNum));
     }
 
-    private IEnumerator Respawn(float _time, GameObject gameObject)
+    IEnumerator RespawnItem(float _time, Vector3 _pos, int _num)
     {
-        foreach(var render in renderlist)
-            render.enabled = false;
-
+        //指定時間分待つ
         yield return new WaitForSecondsRealtime(_time);
 
-        foreach (var render in renderlist)
-            render.enabled = true;
+        //ランダムで再生成
+        Instantiate(ItemPrefab[isRandom ? Random.Range(0, ItemPrefab.Count) : _num], _pos, Quaternion.identity);
     }
+}
+
+public enum ItemState
+{
+    SpeedUp,    //スピードアップ
+    HighJump,   //ハイジャンプ
+    Blowing,    //周囲を吹き飛ばし
+    Stun,       //全体スタン
+    Random,     //ランダム
+    Slow        //全体スロー
+}
+
+public static class Tag
+{
+    public const string PlayerTag = "Player";
 }
