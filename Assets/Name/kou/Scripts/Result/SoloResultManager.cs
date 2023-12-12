494d6d89118a8bc62a80daf2e39201ec22f5d66b
kou <22ci0314@jec.ac.jp>
2023-12-12 14:47:15 +0900

    一人用リザルト

--- Assets/Name/kou/Scripts/Result/SoloResultManager.cs
+++ Assets/Name/kou/Scripts/Result/SoloResultManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,10 +12,10 @@ public class SoloResultManager : MonoBehaviour
     [SerializeField]
     private ResultData[] resultData;
 
-    [SerializeField]
-    Text rankPlayerText;
-    [SerializeField]
-    Text rankTimeText;
+
+    [SerializeField] TextMeshProUGUI minText;
+    [SerializeField] TextMeshProUGUI secText;
+    [SerializeField] TextMeshProUGUI digText;
 
     void Start()
     {
@@ -22,13 +23,57 @@ public class SoloResultManager : MonoBehaviour
         gameManager = playerInputManager.GetComponent<GameManager>();
         resultData = GameManager.Instance.GetResultData().ToArray();
 
-        RankProcess();
+        timeProcess();
     }
 
-    private void RankProcess()
+
+    private void timeProcess()
     {
-        rankPlayerText.text = "Player" + resultData[0].GetPlayerNum().ToString();
-        rankTimeText.text = resultData[0].GetScoreTime().ToString();
+        int timeInt = (int)resultData[0].GetScoreTime();
+        int minute = timeInt / 60;
+        int second = timeInt - minute;
+        float decimalPoint = resultData[0].GetScoreTime() - timeInt;
+
+        minText.SetText(ConvFoolCoolFont(minute.ToString().PadLeft(2, '0')));
+        secText.SetText(ConvFoolCoolFont(second.ToString().PadLeft(2, '0')));
+        digText.SetText(ConvFoolCoolFont(decimalPoint.ToString().Substring(2, 2)));
+    }
+
+    public static string ConvFoolCoolFont(string str)
+    {
+        string rtnStr = "";
+        if (str == null)
+            return rtnStr;
+        for (int i = 0; i < str.Length; i++)
+        {
+            string convStr;
+            switch (str[i])
+            {
+                case 's':
+                    convStr = "10";
+                    break;
+                case 'e':
+                    convStr = "11";
+                    break;
+                case 'c':
+                    convStr = "12";
+                    break;
+                case 'm':
+                    convStr = "13";
+                    break;
+                case 'i':
+                    convStr = "14";
+                    break;
+                case 'n':
+                    convStr = "15";
+                    break;
+                default:
+                    convStr = str[i].ToString();
+                    break;
+            }
+            rtnStr += "<sprite=" + convStr + ">";
+        }
+        return rtnStr;
     }
 
     public void LoadToLobby()
